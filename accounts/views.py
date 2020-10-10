3bf290d794cab60a97d3417c2806a89d74500122
cesar rodriguez <cesarrodriguezpro@gmail.com>
2020-10-10 15:02:09 -0400

    add messeges and fix button for template

--- accounts/views.py
+++ accounts/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from .forms import UserCreateForm
 from django.views.generic import CreateView
+from django.contrib.messages.views import SuccessMessageMixin
 
 
-class SignUp(CreateView):
+class SignUp(SuccessMessageMixin, CreateView):
     form_class = UserCreateForm
     success_url = reverse_lazy('accounts:login')
     template_name = 'accounts/signup.html'
+    success_message = 'You Succesfully Sign Up'
 
